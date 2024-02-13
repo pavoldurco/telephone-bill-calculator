a6640238b137e48bb0f22688a4f9bd6774ad48bb
pavoldurco <pavol.durco@yoso.fi>
2024-02-13 16:47:41 +0100

    Add PricingPolicy

---
+++ src/main/java/com/phonecompany/billing/PricingPolicy.java
@@ -0,0 +1,13 @@
+package com.phonecompany.billing;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+public class PricingPolicy {
+    public static final BigDecimal DAY_RATE = new BigDecimal("1.00");
+    public static final BigDecimal NIGHT_RATE = new BigDecimal("0.50");
+    public static final BigDecimal LONG_CALL_RATE = new BigDecimal("0.20");
+    public static final int LONG_CALL_MINUTES_THRESHOLD = 5;
+    public static final LocalTime DAY_START = LocalTime.of(8, 0);
+    public static final LocalTime DAY_END = LocalTime.of(16, 0);
+}
